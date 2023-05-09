583c53c4bb0de206a8b0f647cd01b9b30cf632e4
Hafiz <a@a.com>
2023-05-09 09:25:40 +0400

    birinci

---
+++ Darkweb/js/script.js
@@ -0,0 +1,144 @@
+// const change = {
+// linkeng:['Home','Benefits','chapters','Princing','Author','Achievements','Reviews','Contact'],
+// linkaz:[ 'Ev' ,'gelirler','bolmeler','qiymetler','muellif','naliyyetler','gorunus','elaqe'],
+// buttoneng:['Subscribe'],
+// buttonaz:['Isarele'],
+// h3eng:['S MAKE THE BEST INVESTMENT','WHY WONTED?'],
+// h3az:['malliye yatir','Ne ucun wonted'],
+// h1eng:['There Is No Friend As Loyal As A Book','What youll achieve by this book'],
+// h1az:['Kitab kimi sadiq dost yoxdur','bu kitabla ne elde edeceksen'],
+// h5eng:['Card title','Card title','Card title','Card title'],
+// h5az:['card basligi','card basligi','card basligi','card basligi'],
+// inputeng:['your email'],
+// inputaz:['senin emailin'],
+// };
+
+
+
+
+document.querySelector('.value').onclick=()=>{
+
+if (document.querySelector('#biz').className === 'light') {
+   document.querySelector('#biz').className ='dark';
+   document.querySelector('.value').innerHTML="light"
+
+   
+
+}else{
+   document.querySelector('#biz').className ='light';
+   document.querySelector('.value').innerHTML="dark"
+   
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
